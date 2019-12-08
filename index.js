0ee75e2f8d2e16eb8e34b2637868e4b25806c2a2
Kevin Tam <kevin@kevintam.dev>
2019-12-07 19:43:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let count = 0
+let timerOn = false
+let timeLeft = 1
+
+function increment() {
+    if (!timerOn) {
+        startTimer()
+    }
+
+    count++
+    document.getElementById('clicks').innerHTML = `${count} Clicks`
+}
+
+function startTimer() {
+    timerOn = true
+
+    setInterval( () => {
+        if (timeLeft > 0) {
+            document.getElementById('timer').innerHTML = `${timeLeft--} seconds left`
+        } else {
+            document.getElementById('click-button').setAttribute('disabled', true)
+            document.getElementById('timer').innerHTML = "Time's up!"
+        }
+    }, 1000)
+
+}
