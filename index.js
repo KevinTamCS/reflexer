1426b0cd4eabd3c31246a50f860d43dfb012a4c8
KevinTamCS <kevin@kevintam.dev>
2020-06-25 01:49:52 -0400

    Center button and add copyright

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-let count = 0
-let timerOn = false
-let timeLeft = 60
+let count = 0;
+let timerOn = false;
+let timeLeft = 60;
 
 function increment() {
-    if (!timerOn) {
-        startTimer()
-    }
+  if (!timerOn) {
+    startTimer();
+  }
 
-    count++
-    document.getElementById('clicks').innerHTML = `${count} Clicks`
+  count++;
+  document.getElementById("clicks").innerHTML = `${count} Clicks`;
 }
 
 function startTimer() {
-    timerOn = true
+  timerOn = true;
 
-    setInterval( () => {
-        if (timeLeft > 0) {
-            document.getElementById('timer').innerHTML = `${timeLeft--} seconds left`
-        } else {
-            document.getElementById('click-button').setAttribute('disabled', true)
-            document.getElementById('timer').innerHTML = "Time's up!"
-        }
-    }, 1000)
-
+  setInterval(() => {
+    if (timeLeft > 0) {
+      document.getElementById("timer").innerHTML = `${timeLeft--} seconds left`;
+    } else {
+      document.getElementById("click-button").setAttribute("disabled", true);
+      document.getElementById("timer").innerHTML = "Time's up!";
+    }
+  }, 1000);
 }
